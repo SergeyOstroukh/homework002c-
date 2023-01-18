215816f66e1b9c47f0a6dc43642cd888d0ae26f7
Sergey <hardmass3ffect@gmail.com>
2023-01-19 00:30:46 +0300

    сделал дз

---
+++ number013/Program.cs
@@ -0,0 +1,29 @@
+﻿// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// // 645 -> 5
+
+// // 78 -> третьей цифры нет
+
+// // 32679 -> 6
+
+
+int number = new Random().Next(-100000, 100000);
+Console.WriteLine($"Получено число {number}");
+if (number < 0)
+    number = (number < 0 ? -number : number);
+if (number < 100)
+    Console.WriteLine($"У числа {number} нет третьей цифры.");
+else
+{
+    int FindDigit(int arg)
+    {
+        int result = 0;
+        while (arg > 999)
+            arg = arg / 10;
+        result = arg % 10;
+        return result;
+    }
+    Console.Write("третья цифра полученного числа -> ");
+    Console.WriteLine(FindDigit(number));
+}
---
+++ number015/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 7 | number < 1)
+    Console.WriteLine("Введите корректное значение дня недели, от 1 до 8");
+else
+{
+Console.Write($"День недели {number},  ");
+bool Day(int arg)
+{
+    bool result = false;
+    if (arg == 6 | arg == 7)
+        result = true;
+    else
+        result = false;
+    return result;
+}
+Console.WriteLine($"выходной? -> {Day(number)}");
+}
